67d0661cc8eeff44dcca6b5abb36f3623011cde4
Alexey Borodenko <kosunix@gmail.com>
2017-11-23 20:40:29 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from pyfolio.timeseries import rolling_fama_french
+
+
+def create_returns_plot():
+    returns = pd.read_csv('SB1(daily_returns).csv', index_col=[0], parse_dates=True)
+
+	rolling_fama_french(returns)
+    
+
+if __name__ == "__main__":
+    create_returns_plot()
